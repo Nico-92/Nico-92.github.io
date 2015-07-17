a4e22a78be9a25f85bc5f6a180a3457175028481
Nico <ntresoldi@remedia.it>
2015-07-17 17:51:26 +0200

    Add education cards

---
+++ dist/js/personal.js
@@ -0,0 +1,24 @@
+$("#unipdCoursesButton").click(function() {
+	$('#unipdProjectsButton').text('PROJECTS');
+	$("#unipdProjects").hide('fold', 100);
+	$(this).hide('fade', 400, function() {
+		$(this).text(function(i, text) {
+			return text === "COURSES" ? 'CLOSE' : "COURSES";
+		})
+		$(this).show('fade', 400);
+	});
+
+	$("#unipdCourses").toggle('fold', 1000);
+});
+
+$("#unipdProjectsButton").click(function() {
+	$('#unipdCoursesButton').text('COURSES');
+	$("#unipdCourses").hide('fold', 100);
+	$(this).hide('fade', 400, function() {
+		$(this).text(function(i, text) {
+			return text === "PROJECTS" ? 'CLOSE' : "PROJECTS";
+		})
+		$(this).show('fade', 400);
+	});
+	$("#unipdProjects").toggle('fold', 1000);
+});
